f8c81cfb26ec2de9a8b4db5970b7b33fd9304468
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-03-06 17:29:32 -0800

    Release 3.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-HTTPAuth',
-    version='2.7.2',
+    version='3.0.0',
     url='http://github.com/miguelgrinberg/flask-httpauth/',
     license='MIT',
     author='Miguel Grinberg',
