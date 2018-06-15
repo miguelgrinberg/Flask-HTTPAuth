a310b78db2b947ab70f3fc35c1a586d822acc7ca
Miguel Grinberg <miguel.grinberg@gmail.com>
2018-06-14 23:23:53 -0700

    remove incorrect references to JWT in example application
    fixes #69

--- examples/token_auth.py
+++ examples/token_auth.py
@@ -2,7 +2,7 @@
 """Token authentication example
 
 This example demonstrates how to protect Flask endpoints with token
-authentication, using JWT tokens.
+authentication, using tokens.
 
 When this application starts, a token is generated for each of the two users.
 To gain access, you can use a command line HTTP client such as curl, passing
@@ -10,23 +10,23 @@ one of the tokens:
 
     curl -X GET -H "Authorization: Bearer <jwt-token>" http://localhost:5000/
 
-The response should include the username, which is obtained from the JWT token.
+The response should include the username, which is obtained from the token.
 """
 from flask import Flask, g
 from flask_httpauth import HTTPTokenAuth
-from itsdangerous import TimedJSONWebSignatureSerializer as JWT
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top secret!'
-jwt = JWT(app.config['SECRET_KEY'], expires_in=3600)
+token_serializer = Serializer(app.config['SECRET_KEY'], expires_in=3600)
 
 auth = HTTPTokenAuth('Bearer')
 
 
 users = ['john', 'susan']
 for user in users:
-    token = jwt.dumps({'username': user})
+    token = token_serializer.dumps({'username': user}).decode('utf-8')
     print('*** token for {}: {}\n'.format(user, token))
 
 
@@ -34,7 +34,7 @@ for user in users:
 def verify_token(token):
     g.user = None
     try:
-        data = jwt.loads(token)
+        data = token_serializer.loads(token)
     except:  # noqa: E722
         return False
     if 'username' in data:
