31a1d4b27ec0f689c052cb6b96b6cf2f402f0604
Miguel Grinberg <miguel.grinberg@gmail.com>
2019-05-19 11:24:55 +0100

    Release 3.3.0

--- flask_httpauth.py
+++ flask_httpauth.py
@@ -15,7 +15,7 @@ from flask import request, make_response, session
 from werkzeug.datastructures import Authorization
 from werkzeug.security import safe_str_cmp
 
-__version__ = '3.2.4'
+__version__ = '3.3.0'
 
 
 class HTTPAuth(object):
