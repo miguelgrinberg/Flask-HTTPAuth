9befbc29922dbfd122397581e469bd8e37e926c8
Saif Almansoori <smm.almansoori@gmail.com>
2019-12-11 02:14:35 +0400

    Add login_optional decorator

--- flask_httpauth.py
+++ flask_httpauth.py
@@ -105,6 +105,22 @@ class HTTPAuth(object):
             return f(*args, **kwargs)
         return decorated
 
+    def login_optional(self, f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            auth = self.get_auth()
+
+            # Flask normally handles OPTIONS requests on its own, but in the
+            # case it is configured to forward those to the application, we
+            # need to ignore authentication headers and let the request through
+            # to avoid unwanted interactions with CORS.
+            if request.method != 'OPTIONS':  # pragma: no cover
+                password = self.get_auth_password(auth)
+                self.authenticate(auth, password)
+
+            return f(*args, **kwargs)
+        return decorated
+
     def username(self):
         if not request.authorization:
             return ""
