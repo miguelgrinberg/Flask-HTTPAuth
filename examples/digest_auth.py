4b9350dbcd9f2f69be4c590adaed02f73f9343e6
homigrotas <shaninadav10@gmail.com>
2021-12-27 15:38:15 +0200

    added warning from flask cookie-based sessions

--- examples/digest_auth.py
+++ examples/digest_auth.py
@@ -7,7 +7,15 @@ authentication. The password is encrypted using a challenge to client.
 After running this example, visit http://localhost:5000 in your browser. To
 gain access, you can use (username=john, password=hello) or
 (username=susan, password=bye).
+
+
+Warning:
+    To make the authentication flow secure when using session storage (which digest auth uses),
+    it is required that server-side sessions are used instead of the default
+    Flask cookie-based sessions, as this ensures that the challenge data is not
+    at risk of being captured as it moves in a cookie between server and client.
 """
+
 from flask import Flask
 from flask_httpauth import HTTPDigestAuth
 from flask_session import Session
